451b0aa80c7d47076137bd57d8c01d273165d024
Ashish Yadav <ashishyadav@glynk.com>
2021-02-25 15:34:06 +0530

    Initial Commit

---
+++ StockAppMVVMProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StockAppMVVMProject
+//
+//  Created by Ashish Yadav on 25/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StockAppMVVMProject/StockAppMVVMProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockAppMVVMProjectApp.swift
+//  StockAppMVVMProject
+//
+//  Created by Ashish Yadav on 25/02/21.
+//
+
+import SwiftUI
+
+@main
+struct StockAppMVVMProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

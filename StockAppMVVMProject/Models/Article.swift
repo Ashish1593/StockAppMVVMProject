4df6e41efdd7ee7351fb467fd475afb7f5a806e9
Ashish Yadav <ashishyadav@glynk.com>
2021-02-26 16:59:59 +0530

    added get articles in webservice

---
+++ StockAppMVVMProject/Models/Article.swift
@@ -0,0 +1,14 @@
+//
+//  Article.swift
+//  StockAppMVVMProject
+//
+//  Created by Ashish Yadav on 26/02/21.
+//
+
+import Foundation
+
+struct Article:Codable {
+    let title:String
+    let publication: String
+    let imageURL: String
+}
--- StockAppMVVMProject/Service/WebService.swift
+++ StockAppMVVMProject/Service/WebService.swift
@@ -8,6 +8,28 @@
 import Foundation
 
 class WebService {
+    
+    func getArticles(completion: @escaping (([Article]?)-> Void)) {
+        
+        guard let url = URL(string: "https://island-bramble.glitch.me/top-news") else {
+            fatalError("Url is not correct")
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data, error == nil else {
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+                return
+            }
+            
+            let articles = try? JSONDecoder().decode([Article].self, from: data)
+            DispatchQueue.main.async {
+                completion(articles)
+            }
+        }.resume()
+    }
+    
     func getStocks(completion: @escaping (([Stock]?)-> Void)) {
         guard let url = URL(string: "https://island-bramble.glitch.me/stocks") else {
             fatalError("Url is not correct")
@@ -15,12 +37,16 @@ class WebService {
         
         URLSession.shared.dataTask(with: url) { (data, response, error) in
             guard let data = data, error == nil else {
-                completion(nil)
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
                 return
             }
             
             let stocks = try? JSONDecoder().decode([Stock].self, from: data)
-            stocks == nil ? completion(nil): completion(stocks)
+            DispatchQueue.main.async {
+                completion(stocks)
+            }
         }.resume()
     }
 }
